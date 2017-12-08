e7d11302ac3e06092e19ce1d47ca292048203288
tibbiustin <tibe.kinda@yahoo.ro>
2017-12-08 20:31:06 +0100

    first

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,21 +1,61 @@
 package com.example.demo;
 
+import com.example.demo.User;
+import com.example.demo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.data.repository.CrudRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.RequestWrapper;
+import java.io.IOException;
 
 /**
- * Created by coag on 19-10-2017.
+ * Created by George Stratulat on 23/11/2017.
  */
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserRepository ur;
+
     @GetMapping("/")
-    public String index(){
-        return "index";
+    public ModelAndView doHome() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
     }
 
-    @GetMapping("/hi")
-    public String hi(){
-        return "hi";
+    @GetMapping("/logOut")
+    public ModelAndView logOut(HttpServletRequest request, HttpServletResponse response) {
+        request.getSession().invalidate();
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+
+    @RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
+    public ModelAndView doLoginCheck(@RequestParam("authCode") int authCode, HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView mv = new ModelAndView("index");
+
+        User user = new User();
+        user = ur.findOne(authCode);
+
+        if (user != null) {
+            request.getSession().setAttribute("userId", authCode);
+
+            if (user.type.equals("student")) {
+                mv.setViewName("redirect:/student");
+            } else if (user.type.equals("teacher")) {
+                mv.setViewName("redirect:/teacher");
+            } else if (user.type.equals("admin")) {
+                mv.setViewName("redirect:/admin");
+            }
+        }
+
+        return mv;
     }
 }
---
+++ src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Tibi on 08/12/2017.
+ */
+public interface UserRepository extends CrudRepository<User, Integer>{
+}
